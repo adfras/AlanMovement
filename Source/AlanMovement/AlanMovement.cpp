6a47443642703d43797e32e574797104d5062800
adfras <adfras@gmail.com>
2025-03-08 16:51:51 +0800

    Initial commit

---
+++ Source/AlanMovement/AlanMovement.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AlanMovement.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, AlanMovement, "AlanMovement");
+
+// Define the log category here.
+DEFINE_LOG_CATEGORY(LogOculusXRMovementSample);
